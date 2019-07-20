daa8ffc5474d71227fc169e59f1466c959132270
sam-malanchuk <malanchuksam@gmail.com>
2019-07-20 12:58:16 -0400

    created the initialState in reducer and applied it to the Store, added thunk and logger in the middleware

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,17 +2,16 @@
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+
+//  Your initial/default state for this project could *Although does not have to* look a lot like this
+const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
+   error: null,
  }
-*/
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +20,11 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default function(state = initialState, action) {
+	switch (action.type) {
+        //  insert reducer cases here
+        default:
+            return state
+    }
+}
